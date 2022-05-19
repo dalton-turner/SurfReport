c2d4c0758414275aecd8f801b53a53b2dae32445
Dalton Turner <dalton@squareup.com>
2022-05-19 09:58:20 -0700

    Initial Commit

---
+++ SurfReport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfReport
+//
+//  Created by Dalton Turner on 5/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
