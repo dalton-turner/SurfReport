5aa59b3ea2327e76c366d38310dc9c3ad8f03c52
Dalton Turner <78099245+dalton-turner@users.noreply.github.com>
2022-05-23 13:10:00 -0700

    Adding title to main VC (#10)

--- SurfReport/ViewController.swift
+++ SurfReport/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        self.title = "Surf Reports"
         view.addSubview(tableView)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.delegate = self
