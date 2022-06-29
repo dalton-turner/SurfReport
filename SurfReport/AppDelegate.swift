31ea9f6a62d3c6d7df247b6fa34b217d306c3459
Dalton Turner <78099245+dalton-turner@users.noreply.github.com>
2022-06-29 11:48:38 -0700

    Adding a login screen and transition to tableview (#15)

--- SurfReport/AppDelegate.swift
+++ SurfReport/AppDelegate.swift
@@ -12,13 +12,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    let loginViewController = LoginViewController()
+    let spotViewController = SpotViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = ViewController()
         
+        loginViewController.delegate = self
+        window?.rootViewController = loginViewController
         return true
+    }
+}
+
+extension AppDelegate: LoginViewControllerDelegate {
+    func didLogIn() {
+        print("logged in")
+        setRootViewController(spotViewController)
+    }
+}
+
+extension AppDelegate {
+    func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+        
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(
+            with: window,
+            duration: 1,
+            options: .transitionCurlUp,
+            animations: nil,
+            completion: nil)
     }
 }
